77d166db7580571782431b2d064bef12d7481456
vikoms <vikoms95@gmail.com>
2019-09-05 12:37:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sqlitepwpb/PersonBean.java
@@ -0,0 +1,25 @@
+package com.example.sqlitepwpb;
+
+public class PersonBean {
+    String name;
+    int age;
+
+    public PersonBean() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
